6932c17e69e76d56e1fc51f64a11c39726186657
mfrizqi <muhammadfajarrizqi@gmail.com>
2019-08-26 12:12:18 +0700

    Create new login page

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://ebinwasdal.sudinkesjp.id/';
+// $config['base_url'] = 'https://ebinwasdal.sudinkesjp.id/';
+$config['base_url'] = 'http://localhost/ebinwasdal-web/';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/login.php
+++ application/views/login.php
@@ -11,10 +11,15 @@
 
   <!-- CSS -->
   <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
-  <link rel="stylesheet" href="<?php echo base_url(); ?>asset/assets/bootstrap/css/bootstrap.min.css">
+  <!-- <link rel="stylesheet" href="<?php echo base_url(); ?>asset/assets/bootstrap/css/bootstrap.min.css"> -->
   <link rel="stylesheet" href="<?php echo base_url(); ?>asset/assets/font-awesome/css/font-awesome.min.css">
   <link rel="stylesheet" href="<?php echo base_url(); ?>asset/assets/css/form-elements.css">
   <link rel="stylesheet" href="<?php echo base_url(); ?>asset/assets/css/style.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>asset/assets/css/utilities.css">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -31,68 +36,42 @@
 
 </head>
 
-<!-- <body background="<?php echo base_url(); ?>asset/Logo/1.jpg"> -->
+<body class="h-100vh">
 
   <!-- Top content -->
-  <div class="top-content">
-    <div class="inner-bg">
-      <div class="container">
-        <div class="row">
-          
-        </div>
-        <!-- <div class="row">
-          <div class="col-sm-8 col-sm-offset-2 text">
-            <img src="<?php echo base_url(); ?>asset/Logo/Logo DKI.png" width="100" height="100" class="img-rounded">
-        <img src="<?php // echo base_url(); ?>asset/Logo/logo_sudinkes.png" width="100" height="100" class="img-rounded">
-            <h1><strong>E - Binwasdal</strong> </h1>
-            <h1>Suku Dinas Kesehatan Jakarta Pusat</h1>
-            <div class="description">
-              <p>
-                Jl. Percetakan Negara Jakarta Pusat, Jakarta
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-sm-6 col-sm-offset-3 form-box">
-            <div class="form-top">
-              <div class="form-top-left">
-                <h3>User Login</h3>
-                <p>silahkan login menggunakan Username dan password</p>
-              </div>
-              <div class="form-top-right">
-                <i class="fa fa-key"></i>
-              </div>
-            </div>
-            <div class="form-bottom">
-              <?php echo form_open_multipart('Login/Home'); ?>
-              <form id="form-login" class="login-form" name="login" method="post" action="cek_login.php" onSubmit="return validasi(this)">
-                <div class="form-group">
-                  <label class="sr-only" for="form-username">Username</label>
-                  <input type="text" name="UserID" placeholder="Username..." class="form-username form-control" id="form-username">
-                </div>
-                <div class="form-group">
-                  <label class="sr-only" for="form-password">Password</label>
-                  <input type="password" name="Password" placeholder="Password..." class="form-password form-control" id="form-password">
-                </div>
-                <?php echo $Error_Msg ?>
-                <button type="submit" class="btn">Login</button>
-              </form>
-              <?php echo form_close(); ?>
-            </div>
-          </div>
-        </div>
-
-        <div class="row"> -->
-          <!-- <div class="col-sm-6 col-sm-offset-3 social-login">
-            <strong>Copyright &copy; 2019 <a href="https://www.instagram.com/nugraharahmat.h/?utm_source=ig_profile_share&igshid=1q4yijc5jzeog" target="_blank">Nugraha Rahmat H</a>.</strong> All rights
-            reserved. -->
-          </div>
-        </div>
-      </div>
+ <div class="container-fluid">
+   <div class="row h-100vh">
+     <!-- WELCOME -->
+     <div class="col-6 my-auto" background="<?php echo base_url(); ?>asset/Logo/1.jpg">
+       <h2>E-BINWASDAL</h2>
     </div>
-
-  </div>
+     <!-- FORM -->
+     <div class="col-6 my-auto">
+       <div class="row h-100vh">
+         <div class="col-md-10 my-auto">
+           <div>
+                <form>
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  </div>
+                  <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                  </div>
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+         </div>
+       </div>
+     </div>
+   </div>
+ </div>
 
 
   <!-- Javascript -->
@@ -100,6 +79,12 @@
   <script src="<?php echo base_url(); ?>asset/assets/bootstrap/js/bootstrap.min.js"></script>
   <script src="<?php echo base_url(); ?>asset/assets/js/jquery.backstretch.min.js"></script>
   <script src="<?php echo base_url(); ?>asset/assets/js/scripts.js"></script>
+
+  <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
   <!--[if lt IE 10]>
             <script src="assets/assets/js/placeholder.js"></script>
